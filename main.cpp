f5329905b40afc689d6d7b243717ed734a8d1998
nada <nadaamiin00@gmail.com>
2024-11-25 16:01:49 +0200

    testing

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -8,22 +9,53 @@ class set{
 private:
     vector<T> vec;
 public:
-    bool exist(T& element) const{
-        for (auto it = vec.begin(); it != vec.end() - 1; ++it) {
-            if(*it == element)
-                return true;
-        }
-        return false;
+    bool exist(const T& element) const{
+        return any_of(vec.begin(), vec.end(), [&](const T& i) { return i == element; });
     }
-    void add(T& element){
+    void add(const T& element){
         if(!exist(element))
             vec.push_back(element);
     }
-    void remove(T& element){
-        vec.pop_back(element);
+    void remove(const T& element){
+        for (auto it = vec.begin(); it != vec.end(); ++it) {
+            if(*it == element){
+                vec.erase(it);
+                return;
+            }
+        }
+    }
+    int size() const{
+        return vec.size();
+    }
+    T* array() const{
+        T* arr = new T(vec.size());
+        for (int i = 0; i < vec.size(); ++i) {
+            arr[i] = vec[i];
+        }
+        return arr;
+    }
+    void print() const{
+        for (const auto& it : vec) {
+            cout << it << " ";
+        }
+        cout << endl;
     }
 };
 int main(){
+    set<double> s;
+    s.add(6.6);
+    s.add(7);
+    cout << s.size() << endl;
+    s.remove(8);
+    s.print();
+    cout << s.size() << endl;
 
+    double* arr = s.array();
+    cout << "Array contents: ";
+    for (int i = 0; i < s.size(); ++i) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
 
+    delete[] arr; // Free allocated memory
 }
