d8109e66d72b3b11241ad505b4b31578c92b27fa
nada <nadaamiin00@gmail.com>
2024-11-25 15:23:48 +0200

    testing

--- main.cpp
+++ main.cpp
@@ -8,8 +8,19 @@ class set{
 private:
     vector<T> vec;
 public:
-    set(){
-
+    bool exist(T& element) const{
+        for (auto it = vec.begin(); it != vec.end() - 1; ++it) {
+            if(*it == element)
+                return true;
+        }
+        return false;
+    }
+    void add(T& element){
+        if(!exist(element))
+            vec.push_back(element);
+    }
+    void remove(T& element){
+        vec.pop_back(element);
     }
 };
 int main(){
